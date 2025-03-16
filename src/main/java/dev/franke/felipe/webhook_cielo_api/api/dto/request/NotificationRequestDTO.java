c48cfb357ac17c0f857054ed9f82d6ca24c68a5a
FelipeFranke5 <frankefelipee@gmail.com>
2025-03-16 18:54:18 +0000

    feat(NotificationRequestDTO): add new record for notification request data

---
+++ src/main/java/dev/franke/felipe/webhook_cielo_api/api/dto/request/NotificationRequestDTO.java
@@ -0,0 +1,11 @@
+package dev.franke.felipe.webhook_cielo_api.api.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record NotificationRequestDTO(
+    @JsonProperty("PaymentId") String paymentId,
+    @JsonProperty("RecurrentPaymentId") String recurrentPaymentId,
+    @JsonProperty("ChangeType") int changeType
+) {
+
+}
