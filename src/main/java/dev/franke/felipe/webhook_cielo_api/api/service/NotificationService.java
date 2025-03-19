477be82cbb8956303cbb1ce85d9c5295c7512152
FelipeFranke5 <frankefelipee@gmail.com>
2025-03-18 22:52:16 -0300

    refactor: make NotificationRepository final

--- src/main/java/dev/franke/felipe/webhook_cielo_api/api/service/NotificationService.java
+++ src/main/java/dev/franke/felipe/webhook_cielo_api/api/service/NotificationService.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class NotificationService {
 
-    private NotificationRepository notificationRepository;
+    private final NotificationRepository notificationRepository;
 
     public NotificationService(NotificationRepository notificationRepository) {
         this.notificationRepository = notificationRepository;
