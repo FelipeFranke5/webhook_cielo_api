7e1f1fc3be80840634424418c40a0d3ac7fff91f
FelipeFranke5 <frankefelipee@gmail.com>
2025-03-19 00:54:38 -0300

    feat: include a test for Kafka on startup

--- src/main/java/dev/franke/felipe/webhook_cielo_api/WebhookCieloApiApplication.java
+++ src/main/java/dev/franke/felipe/webhook_cielo_api/WebhookCieloApiApplication.java
@@ -1,14 +1,30 @@
 package dev.franke.felipe.webhook_cielo_api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 
 @SpringBootApplication
 public class WebhookCieloApiApplication {
 
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
+
 	public static void main(String[] args) {
 		SpringApplication.run(WebhookCieloApiApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner initial() {
+		return args -> {
+			System.out.println("Testing KAKFA ..");
+			kafkaTemplate.send("webhook-cielo", "testing..");
+			kafkaTemplate.send("webhook-cielo", "testing again..");
+		};
 	}
 
 }
