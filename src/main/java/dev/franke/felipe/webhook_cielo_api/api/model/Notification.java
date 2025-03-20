29992a24358d2ae0df646a00a73f8c1d3d63b0a5
FelipeFranke5 <frankefelipee@gmail.com>
2025-03-20 09:58:02 -0300

    refactor: change ChangeType to Object Integer

--- src/main/java/dev/franke/felipe/webhook_cielo_api/api/model/Notification.java
+++ src/main/java/dev/franke/felipe/webhook_cielo_api/api/model/Notification.java
@@ -5,103 +5,102 @@ import dev.franke.felipe.webhook_cielo_api.api.exception.InvalidNotificationExce
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.util.Objects;
 import java.util.UUID;
 
 @Entity
 @Table(name = "notification")
 public class Notification {
 
-  public static Notification fromRequest(NotificationRequestDTO request) {
-    try {
-      String paymentId = request.paymentId();
-      String recurrent = request.recurrentPaymentId();
-      if (paymentId != null) {
-        UUID.fromString(paymentId);
-      } else {
-        throw new InvalidNotificationException("Invalid payload: PaymentId is required");
-      }
-      if (recurrent != null) {
-        UUID.fromString(recurrent);
-      }
-    } catch (IllegalArgumentException illegalArgumentException) {
-      throw new InvalidNotificationException(
-          "Invalid payload: PaymentId/RecurrentPaymentId should be UUID");
+    public static Notification fromRequest(NotificationRequestDTO request) {
+        try {
+            String paymentId = request.paymentId();
+            String recurrent = request.recurrentPaymentId();
+            if (paymentId != null) {
+                UUID.fromString(paymentId);
+            } else {
+                throw new InvalidNotificationException("Invalid payload: PaymentId is required");
+            }
+            if (recurrent != null) {
+                UUID.fromString(recurrent);
+            }
+        } catch (IllegalArgumentException illegalArgumentException) {
+            throw new InvalidNotificationException("Invalid payload: PaymentId/RecurrentPaymentId should be UUID");
+        }
+        return new Notification(request.paymentId(), request.recurrentPaymentId(), request.changeType());
     }
-    return new Notification(
-        request.paymentId(), request.recurrentPaymentId(), request.changeType());
-  }
 
-  @Id private String paymentId;
+    @Id
+    private String paymentId;
 
-  private String recurrentPaymentId;
-  private int changeType;
+    private String recurrentPaymentId;
+    private Integer changeType;
 
-  public Notification() {}
+    public Notification() {}
 
-  public Notification(String paymentId, String recurrentPaymentId, int changeType) {
-    this.paymentId = paymentId;
-    this.recurrentPaymentId = recurrentPaymentId;
-    this.changeType = changeType;
-  }
+    public Notification(String paymentId, String recurrentPaymentId, int changeType) {
+        this.paymentId = paymentId;
+        this.recurrentPaymentId = recurrentPaymentId;
+        this.changeType = changeType;
+    }
 
-  public String getPaymentId() {
-    return paymentId;
-  }
+    public String getPaymentId() {
+        return paymentId;
+    }
 
-  public void setPaymentId(String paymentId) {
-    this.paymentId = paymentId;
-  }
+    public void setPaymentId(String paymentId) {
+        this.paymentId = paymentId;
+    }
 
-  public String getRecurrentPaymentId() {
-    return recurrentPaymentId;
-  }
+    public String getRecurrentPaymentId() {
+        return recurrentPaymentId;
+    }
 
-  public void setRecurrentPaymentId(String recurrentPaymentId) {
-    this.recurrentPaymentId = recurrentPaymentId;
-  }
+    public void setRecurrentPaymentId(String recurrentPaymentId) {
+        this.recurrentPaymentId = recurrentPaymentId;
+    }
 
-  public int getChangeType() {
-    return changeType;
-  }
+    public int getChangeType() {
+        return changeType;
+    }
 
-  public void setChangeType(int changeType) {
-    this.changeType = changeType;
-  }
+    public void setChangeType(int changeType) {
+        this.changeType = changeType;
+    }
 
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((paymentId == null) ? 0 : paymentId.hashCode());
-    result = prime * result + ((recurrentPaymentId == null) ? 0 : recurrentPaymentId.hashCode());
-    result = prime * result + changeType;
-    return result;
-  }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((paymentId == null) ? 0 : paymentId.hashCode());
+        result = prime * result + ((recurrentPaymentId == null) ? 0 : recurrentPaymentId.hashCode());
+        result = prime * result + changeType;
+        return result;
+    }
 
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj) return true;
-    if (obj == null) return false;
-    if (getClass() != obj.getClass()) return false;
-    Notification other = (Notification) obj;
-    if (paymentId == null) {
-      if (other.paymentId != null) return false;
-    } else if (!paymentId.equals(other.paymentId)) return false;
-    if (recurrentPaymentId == null) {
-      if (other.recurrentPaymentId != null) return false;
-    } else if (!recurrentPaymentId.equals(other.recurrentPaymentId)) return false;
-    if (changeType != other.changeType) return false;
-    return true;
-  }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        Notification other = (Notification) obj;
+        if (paymentId == null) {
+            if (other.paymentId != null) return false;
+        } else if (!paymentId.equals(other.paymentId)) return false;
+        if (recurrentPaymentId == null) {
+            if (other.recurrentPaymentId != null) return false;
+        } else if (!recurrentPaymentId.equals(other.recurrentPaymentId)) return false;
+        return Objects.equals(changeType, other.changeType);
+    }
 
-  @Override
-  public String toString() {
-    return "Notification [paymentId="
-        + paymentId
-        + ", recurrentPaymentId="
-        + recurrentPaymentId
-        + ", changeType="
-        + changeType
-        + "]";
-  }
+    @Override
+    public String toString() {
+        return "Notification [paymentId="
+                + paymentId
+                + ", recurrentPaymentId="
+                + recurrentPaymentId
+                + ", changeType="
+                + changeType
+                + "]";
+    }
 }
