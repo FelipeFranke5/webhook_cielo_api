0fefaf0ac4244ea71e8635f3f0841a4790c62d1d
FelipeFranke5 <frankefelipee@gmail.com>
2025-03-20 09:57:12 -0300

    refactor: change indentation

--- src/main/java/dev/franke/felipe/webhook_cielo_api/api/exception/InvalidNotificationException.java
+++ src/main/java/dev/franke/felipe/webhook_cielo_api/api/exception/InvalidNotificationException.java
@@ -1,7 +1,7 @@
 package dev.franke.felipe.webhook_cielo_api.api.exception;
 
 public class InvalidNotificationException extends RuntimeException {
-  public InvalidNotificationException(String message) {
-    super(message);
-  }
+    public InvalidNotificationException(String message) {
+        super(message);
+    }
 }
--- src/main/java/dev/franke/felipe/webhook_cielo_api/api/exception/InvalidNotificationIdException.java
+++ src/main/java/dev/franke/felipe/webhook_cielo_api/api/exception/InvalidNotificationIdException.java
@@ -2,7 +2,7 @@ package dev.franke.felipe.webhook_cielo_api.api.exception;
 
 public class InvalidNotificationIdException extends RuntimeException {
 
-  public InvalidNotificationIdException(String message) {
-    super(message);
-  }
+    public InvalidNotificationIdException(String message) {
+        super(message);
+    }
 }
