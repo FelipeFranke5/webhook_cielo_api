7376970ce8f5c821ee606e1075cc0e6495c5dccc
FelipeFranke5 <frankefelipee@gmail.com>
2025-03-20 09:58:45 -0300

    feat: add retrieveData by PaymentId

--- src/main/java/dev/franke/felipe/webhook_cielo_api/api/controller/NotificationController.java
+++ src/main/java/dev/franke/felipe/webhook_cielo_api/api/controller/NotificationController.java
@@ -1,25 +1,34 @@
 package dev.franke.felipe.webhook_cielo_api.api.controller;
 
 import dev.franke.felipe.webhook_cielo_api.api.dto.request.NotificationRequestDTO;
+import dev.franke.felipe.webhook_cielo_api.api.dto.response.NotificationResponseDTO;
+import dev.franke.felipe.webhook_cielo_api.api.model.Notification;
 import dev.franke.felipe.webhook_cielo_api.api.service.NotificationService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/webhook")
 public class NotificationController {
 
-  private final NotificationService notificationService;
+    private final NotificationService notificationService;
 
-  public NotificationController(NotificationService notificationService) {
-    this.notificationService = notificationService;
-  }
+    public NotificationController(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
 
-  @PostMapping
-  public ResponseEntity<Void> processData(@RequestBody NotificationRequestDTO payload) {
-    return notificationService.isNotificationSaved(payload) ? ResponseEntity.ok().build() : ResponseEntity.internalServerError().build();
-  }
+    @PostMapping
+    public ResponseEntity<Void> processData(@RequestBody NotificationRequestDTO payload) {
+        return notificationService.isNotificationSaved(payload)
+                ? ResponseEntity.ok().build()
+                : ResponseEntity.internalServerError().build();
+    }
+
+    @GetMapping("{paymentId}")
+    public ResponseEntity<NotificationResponseDTO> retrieveData(@PathVariable String paymentId) {
+        Notification notification = notificationService.getNotificationByPaymentId(paymentId);
+        NotificationResponseDTO responseBody = new NotificationResponseDTO(
+                notification.getPaymentId(), notification.getRecurrentPaymentId(), notification.getChangeType());
+        return ResponseEntity.ok(responseBody);
+    }
 }
