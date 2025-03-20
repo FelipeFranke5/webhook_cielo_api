c3fb972f6b7df22d5bc91a0ab392ad1742c11b45
FelipeFranke5 <frankefelipee@gmail.com>
2025-03-20 10:02:45 -0300

    feat: include new error codes

--- src/main/java/dev/franke/felipe/webhook_cielo_api/api/exception_handler/NotificationErrorCode.java
+++ src/main/java/dev/franke/felipe/webhook_cielo_api/api/exception_handler/NotificationErrorCode.java
@@ -1,17 +1,19 @@
 package dev.franke.felipe.webhook_cielo_api.api.exception_handler;
 
 public enum NotificationErrorCode {
-  INVALID_PAYMENT_OR_RECURRENT_PAYMENT_ID(1),
-  PAYMENT_ID_REQUIRED(2),
-  OTHER(3);
+    INVALID_PAYMENT_OR_RECURRENT_PAYMENT_ID(1),
+    PAYMENT_ID_REQUIRED(2),
+    OTHER(3),
+    PAYMENT_NOT_FOUND(4),
+    INVALID_FORMAT(5);
 
-  private final int code;
+    private final int code;
 
-  NotificationErrorCode(int code) {
-    this.code = code;
-  }
+    NotificationErrorCode(int code) {
+        this.code = code;
+    }
 
-  public int getCode() {
-    return code;
-  }
+    public int getCode() {
+        return code;
+    }
 }
